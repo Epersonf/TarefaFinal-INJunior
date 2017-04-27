093c5707c3178350a798fb0a57c85c8562c09fda
albertdm <betaodm@gmail.com>
2017-04-27 11:08:11 -0300

    v1 ok heroku

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secretKey': '84126-67890-67412-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/ambaya'
-    //'mongoUrl' : process.env.MONGODB_URI
+    //'mongoUrl' : 'mongodb://localhost:27017/ambaya'
+    'mongoUrl' : process.env.MONGODB_URI
 }
