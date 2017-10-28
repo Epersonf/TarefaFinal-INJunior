e3cb0f3b884421657c7b40159a629d70d742d50c
albertdm <betaodm@gmail.com>
2017-10-28 16:27:45 -0200

    autenticação teste2

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+}); */
 
 app.use(express.static(path.join(__dirname, 'public')));
 
