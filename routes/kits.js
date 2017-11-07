6cc2d07308715a706e5f609cb49e5b948ce666aa
albertdm <betaodm@gmail.com>
2017-11-06 22:07:18 -0200

    rotas

---
+++ routes/kits.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var Kit = require('../models/kit');
+
+router.post('/' ,function (req, res, next) {
+    Kit.create(req.body, function (err) {
+        if (err) throw err;
+        res.json("Kit criado com sucesso!");
+    });
+});
+router.get('/' ,function (req, res, next) {
+    Kit.find({}).populate({
+        path: 'consultora',
+        select: 'nome _id',
+        }).exec(function (err, kit) {
+            if (err) throw err;
+            res.json(kit);
+    });
+});
+
+router.get('/porConsultora/:id' ,function (req, res, next) {
+    Kit.find({consultora: req.params.id}).exec(function (err, kit) {
+            if (err) throw err;
+            res.json(kit);
+    });
+});
+
+module.exports = router;
