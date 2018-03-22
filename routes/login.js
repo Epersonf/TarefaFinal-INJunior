da4a7719f70017760351fe60a056614b22a2e632
albert-dm <albert-dm@outlook.com>
2018-03-22 17:08:02 -0300

    Sending password mail

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,17 @@ var passport = require('passport');
 var User = require('../models/user');
 var Verify = require('./verify');
 var uuid = require('uuid');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  host: 'mx1.weblink.com.br',
+  port: 587,
+  secure: false, // upgrade later with STARTTLS
+  auth: {
+    user: 'equipe@ambaya.com.br',
+    pass: 'ambaya2014'
+  }
+})
 
 
 router.post('/', function (req, res, next) {
@@ -38,22 +49,31 @@ router.post('/', function (req, res, next) {
 router.post('/esqueci-senha', function (req, res, next) {
   var state = uuid();
   User.update({ username: req.body.login, email: req.body.email },
-      {
-        $set: {
-          state: state
-        }
-      },
-      function (err, data) {
-        //send email
-        res.json(data);
+    {
+      $set: {
+        state: state
       }
-    );
+    },
+    function (err, data) {
+      var mailOptions = {
+        from: '"Equipe Ambaya" <equipe@ambaya.com.br>',
+        to: req.body.email,
+        subject: 'Recuperação de Senha',
+        html: '<p>Para recuperar sua senha <a href="http://minha.ambaya.com.br/#/recuperar-senha/' + state + '">clique aqui</a></p>' // html body
+      };
+
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, (error, info) => {
+        res.json(data);
+      });
+    }
+  );
 });
 
 router.post('/nova-senha', function (req, res, next) {
   User.findOne({ state: req.body.state },
     function (err, user) {
-      user.setPassword(req.body.senha, function(error){
+      user.setPassword(req.body.senha, function (error) {
         user.state = "";
         user.save();
       });
