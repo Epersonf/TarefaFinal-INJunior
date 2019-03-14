e55cdb12b72d3c417628a045a719e348dc0a75ba
albert-dm <albert-dm@outlook.com>
2019-03-13 22:58:52 -0300

    Consertando erro de calculo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secretKey': '84126-67890-67412-54321',
     'PORT': process.env.PORT || 8080,
-    //'mongoUrl' : process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/ambaya',
+    //'mongoUrl': process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/ambaya',
     'mongoUrl': 'mongodb://heroku_47z4qmjw:21t0mlcgsas4bj7asap5c3h5s0@ds127978.mlab.com:27978/heroku_47z4qmjw',
     'CLOUDINARY_URL': process.env.CLOUDINARY_URL || 'cloudinary://279488126232216:4HBKePMxrgMu3AHRWW6KIyLEbUs@hkzbsdu4z'
 }
