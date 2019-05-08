19d6e0ee69e7cfcbe4a885b8bf76035411781ba2
albertdm <albert-dm@outlook.com>
2019-05-07 21:32:26 -0300

    Trying payment again

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '84126-67890-67412-54321',
     'PORT': process.env.PORT || 8080,
     'mongoUrl': process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/ambaya',
-    //'mongoUrl': 'mongodb://heroku_47z4qmjw:21t0mlcgsas4bj7asap5c3h5s0@ds127978.mlab.com:27978/heroku_47z4qmjw',
+    //'mongoUrl': 'mongodb://albertdm:8xvq9x8z@ds151006-a0.mlab.com:51006,ds151006-a1.mlab.com:51006/heroku_l959zvp6?replicaSet=rs-ds151006',
     'CLOUDINARY_URL': process.env.CLOUDINARY_URL || 'cloudinary://279488126232216:4HBKePMxrgMu3AHRWW6KIyLEbUs@hkzbsdu4z',
     'pagseguroEmail': 'ambayasemijoias@gmail.com',
     'pagseguroSandboxToken': '4B600F906F9C49EFA0520D94ADFA1AEE',
