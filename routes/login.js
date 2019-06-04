f4e349f55ecd24c825000c53f21106251c0c2472
albertdm <albert-dm@outlook.com>
2019-06-04 09:29:02 -0300

    Still trying to fix

--- routes/login.js
+++ routes/login.js
@@ -57,7 +57,7 @@ router.post('/esqueci-senha', function (req, res, next) {
 
 router.post('/nova-senha', async  (req, res, next) => {
   const userToChange = await User.findOne({ state: req.body.state }).exec();
-  await userToChange.setPassword(user.password);
+  await userToChange.setPassword(req.body.senha);
   await userToChange.save();
   res.status(200).json({ msg: 'Password changed' });
 });
