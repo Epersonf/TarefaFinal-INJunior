1bd01a855e165038229b0e6e5eb1a63c6e05f86f
albertdm <albert-dm@outlook.com>
2019-06-13 08:57:41 -0300

    Try again email layout

--- helpers/emailHelper.js
+++ helpers/emailHelper.js
@@ -10,18 +10,20 @@ const getForgottenPasswordEmail = (returnUrl, state) => {
         <title>Ambaya - Esqueci minha senha</title>
     </head>
     
-    <body style="background-color: #FFD44B;display: flex;flex-direction: column;width: 100%;align-items: center;font-family: 'Roboto';">
-        <img src="https://minha.ambaya.com.br/img/logo_barra.png" alt="Ambaya Semijoias"  style=" max-width: 50%;" />
-        <div style="background-color: white;padding: 20px 100px;margin: 10px 15px 30px 15px;text-align: center;border-radius: 3px;box-shadow: 0px 6px 5px 0px rgba(0, 0, 0, 0.75);">
-            <h1 style="font-weight: 300;">Esqueceu a senha?</h1>
-            <p style="font-size: 3em;">🤔</p>
-            <p>Não tem problema, basta clicar no botão abaixo para configurar uma nova senha!</p>
-            </br>
-            <a style="background-color: #E6B000;text-decoration: none;padding: 7px 25px;border-radius: 3px;text-transform: uppercase;color: #ffffff;box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.75);"
-            href="${returnUrl}/${state}">Nova senha</a>
-            </br>
+    <body style="background-color: #FFD44B;width: 100%">
+        <div style="display: flex;flex-direction: column;width: 100%;align-items: center;font-family: 'Roboto';">
+            <img src="https://minha.ambaya.com.br/img/logo_barra.png" alt="Ambaya Semijoias"  style=" max-width: 50%;" />
+            <div style="background-color: white;padding: 20px 100px;margin: 10px 15px 30px 15px;text-align: center;border-radius: 3px;box-shadow: 0px 6px 5px 0px rgba(0, 0, 0, 0.75);">
+                <h1 style="font-weight: 300;">Esqueceu a senha?</h1>
+                <p style="font-size: 3em;">🤔</p>
+                <p>Não tem problema, basta clicar no botão abaixo para configurar uma nova senha!</p>
+                </br>
+                <a style="background-color: #E6B000;text-decoration: none;padding: 7px 25px;border-radius: 3px;text-transform: uppercase;color: #ffffff;box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.75);"
+                href="${returnUrl}/${state}">Nova senha</a>
+                </br>
+            </div>
+            <small>Ambaya Semijoias - 2019</small>
         </div>
-        <small>Ambaya Semijoias - 2019</small>
     </body>
     
     </html>
