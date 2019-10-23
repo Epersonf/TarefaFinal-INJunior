abeae0d597d15e73f752b35eebb79f39784087d9
albert-dm <albert-dm@outlook.com>
2019-10-22 22:22:13 -0300

    Adding setup shell

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Adding server production repo"
+git remote add production https://git.heroku.com/ambaya.git
+echo "Adding server test repo"
+git remote add test https://git.heroku.com/ambaya-teste.git
