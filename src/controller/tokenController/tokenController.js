9b14def26cc02d42874adf380dbf65a9e8f67c0a
EPERSON <epersoncmxf@gmail.com>
2021-01-26 05:52:04 -0300

    Tentativa de corrigir token

--- src/controller/tokenController/tokenController.js
+++ src/controller/tokenController/tokenController.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (params = {}) => {
+async function generateToken(params = {}) {
     return jwt.sign(params,
         process.env.SECRET || "490bc6115cc2db9292b33dbb6cd0285e",
         86400);
